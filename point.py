4e1e99165f307d823d4bbbfe54229d3022904503
andrey <contato.andreyazevedo@gmail.com>
2025-01-28 17:30:34 -0300

    ajustes no points.py

--- point.py
+++ point.py
@@ -32,10 +32,10 @@ def render():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
-    glBegin(GL_POINTS)
+
     glColor3f(1.0, 0.0, 0.0)
     glVertex2f(0.0, 0.9)
-    glEnd()
+
 
     glBegin(GL_LINE_LOOP)
     for v in roof:
